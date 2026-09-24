6c84709c77b93e4e6bad5d996bae79f676948c0c
Praveen Thirumurugan <mail@praveent.com>
2026-09-24 10:41:09 +0530

    Increase entity switcher search radius by one pixel

--- apps/towbar-web-app/src/components/breadcrumb-entity-switcher.tsx
+++ apps/towbar-web-app/src/components/breadcrumb-entity-switcher.tsx
@@ -137,7 +137,7 @@ export function BreadcrumbEntitySwitcher({
             className="px-2 pt-2"
             variant="secondary"
           >
-            <SearchField.Group className="rounded">
+            <SearchField.Group className="rounded-[5px]">
               <SearchField.SearchIcon />
               <SearchField.Input
                 className="text-base sm:text-sm"
